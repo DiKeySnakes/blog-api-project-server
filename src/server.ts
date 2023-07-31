febde2098ec36ddf4d1faafd700a889cf454ba17
DiKeySnakes <dikeysnakes@gmail.com>
2023-07-31 09:32:24 +0300

    Modify src/server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,28 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import 'express-async-errors';
-import express from 'express';
+import express, { Express, Request, Response } from 'express';
+import morgan from 'morgan';
+import compression from 'compression';
+import helmet from 'helmet';
 import errorHandler from 'strong-error-handler';
-
-const app = express();
 
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import corsOptions from './config/corsOptions.js';
 
 import mongoose from 'mongoose';
+
+const app: Express = express();
+
+// Set up rate limiter: maximum of twenty requests per minute
+import RateLimit from 'express-rate-limit';
+const limiter = RateLimit({
+  windowMs: 1 * 10 * 1000, // 10 seconds
+  max: 10,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
 
 main().catch((err) => console.log(err));
 
@@ -24,14 +36,28 @@ async function main() {
   });
 }
 
+app.use(morgan('dev'));
+
 app.use(cors(corsOptions));
 
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: false }));
+
 app.use(cookieParser());
 
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      'script-src': ["'self'", 'code.jquery.com', 'cdn.jsdelivr.net'],
+    },
+  })
+);
+
+app.use(compression()); // Compress all routes
+
 // 404 page
-app.use((req, res) => {
+app.use((req: Request, res: Response) => {
   res.status(404).json({ msg: 'Page Not Found' });
 });
 

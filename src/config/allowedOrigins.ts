8f79626f4e521da141496c829e92d59b2ba80799
DiKeySnakes <dikeysnakes@gmail.com>
2023-08-13 09:47:06 +0300

    Modify src/config/allowedOrigins.ts

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   'http://localhost:5173',
   'https://blog-api-project-client.vercel.app',
+  'https://blog-api-project-client.onrender.com',
 ];
 
 export default allowedOrigins;
